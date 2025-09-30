168cfdd5281f69e800384789cc818beee446c45d
Thomas Riffard <ferdodo@hotmail.fr>
2025-09-30 15:06:18 +0200

    use daily-prng

--- src/randomNumber.ts
+++ src/randomNumber.ts
@@ -1,16 +1,5 @@
-// @ts-ignore
-import seedrandom from "seedrandom";
-const date = new Date();
-const rng = getRng(date);
+import { randomFloat } from "daily-prng";
 
 export function randomNumber(min: number, max: number) {
-	return Math.floor(rng() * (max - min) + min);
-}
-
-function getRng(date: Date) {
-	const year = date.getFullYear();
-	const month = ("0" + (date.getMonth() + 1)).slice(-2);
-	const day = ("0" + date.getDate()).slice(-2);
-	const formattedDate = `${year}/${month}/${day}`;
-	return seedrandom(formattedDate);
+	return Math.floor(randomFloat(min, max));
 }
--- src/randomNumberForYesterday.ts
+++ src/randomNumberForYesterday.ts
@@ -1,16 +1,5 @@
-// @ts-ignore
-import seedrandom from "seedrandom";
-const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000);
-const rngYesterday = getRng(yesterday);
+import { randomFloat } from "daily-prng";
 
 export function randomNumberForYesterday(min: number, max: number) {
-	return Math.floor(rngYesterday() * (max - min) + min);
-}
-
-function getRng(date: Date) {
-	const year = date.getFullYear();
-	const month = ("0" + (date.getMonth() + 1)).slice(-2);
-	const day = ("0" + date.getDate()).slice(-2);
-	const formattedDate = `${year}/${month}/${day}`;
-	return seedrandom(formattedDate);
+	return Math.floor(randomFloat(min, max, -1));
 }
