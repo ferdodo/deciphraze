d67f29eec6b6597ba550581bfe8dfefd987ce863
Thomas Riffard <ferdodo@hotmail.fr>
2025-10-04 19:24:53 +0200

    disable static mutatns

--- src/services/createSymbolSelection.test.ts
+++ src/services/createSymbolSelection.test.ts
@@ -4,14 +4,6 @@ import { createSymbolSelection } from "./createSymbolSelection";
 describe("createSymbolSelection", () => {
 	let symbolSelection: ReturnType<typeof createSymbolSelection>;
 
-	describe("Initialization", () => {
-		it("should start with no selection", () => {
-			symbolSelection = createSymbolSelection();
-			const selected = symbolSelection.getSymbolSelection();
-			expect(selected).toBeNull();
-		});
-	});
-
 	describe("selectSymbol", () => {
 		it("should clear selection with null", () => {
 			symbolSelection = createSymbolSelection();
