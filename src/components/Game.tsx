6fbcbc433583025aa8d0d7bd2b3ba972ac5f387e
Thomas Riffard <ferdodo@hotmail.fr>
2025-10-25 10:21:02 +0200

    call register winned game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import { useMemo } from "react";
+import { useMemo, useEffect } from "react";
 import { paragraphOfTheDay } from "../paragraphOfTheDay";
 import { FragmentComponent } from "./FragmentComponent";
 import { LetterComponent } from "./LetterComponent";
@@ -10,15 +10,25 @@ import { useMatchCount } from "../hooks/useMatchCount";
 import { useWin } from "../hooks/useWin";
 import { share } from "../utils/share";
 import { paragraphOfYesterday } from "../paragraphOfYesterday";
+import { registerWinnedGame } from "../usecases/registerWinnedGame";
+import { useGameContext } from "../contexts/useGameContext";
 
 const Game: React.FC = () => {
+	const context = useGameContext();
+
+    useEffect(() => {
+        registerWinnedGame(context);
+    }, [context]);
+
 	const separatedWords = paragraphOfTheDay.split(" ");
 	const words = separatedWords.map((word) => [...word]);
 	const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
 	const alphabetRandom = useMemo(() => {
 		const CHEAT_MODE = true;
 		return generateRandomAlphabet(CHEAT_MODE);
 	}, []);
+
 	const win = useWin();
 	const matchCount = useMatchCount();
 
