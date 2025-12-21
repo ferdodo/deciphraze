21ac30855db12da6996148de215700514aeb1e48
Thomas Riffard <ferdodo@hotmail.fr>
2025-12-21 11:34:18 +0100

    fix vite build

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import type React from "react";
+import { Game } from "./Game";
+import { GameContextProvider } from "../contexts/GameContextProvider";
+import "crumbs-design-system";
+import styles from "./App.module.css";
+
+
+export const App: React.FC = () => {
+	return (
+		<div className={styles.app}>
+			<GameContextProvider>
+				<Game />
+			</GameContextProvider>
+		</div>
+	);
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
-import type React from "react";
 import { createRoot, type Root } from "react-dom/client";
-import { Game } from "./components/Game";
-import { GameContextProvider } from "./contexts/GameContextProvider";
+import { App } from "./components/App";
 import "crumbs-design-system";
-import "./styles/global.module.css";
+import "./main.module.css";
 
-
-const App: React.FC = () => {
-	return (
-		<GameContextProvider>
-			<Game />
-		</GameContextProvider>
-	);
-};
-
-const container: HTMLElement = document.body;
+const container: HTMLElement = document.createElement("div");
+document.body.appendChild(container);
 const root: Root = createRoot(container);
 root.render(<App />);
