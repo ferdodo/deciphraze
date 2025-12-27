0c8b6a2cfcb12c4fe5aafd5388677a3defe08300
Thomas Riffard <ferdodo@hotmail.fr>
2025-12-27 16:01:53 +0100

    migrate last component

---
+++ ds/src/components/DeciDevelopmentPanel.tsx
@@ -0,0 +1,40 @@
+interface DeciDevelopmentPanelProps {
+	currentDay: string;
+	onIncrementDay: () => void;
+	onDecrementDay: () => void;
+}
+
+export function DeciDevelopmentPanel({
+	currentDay,
+	onIncrementDay,
+	onDecrementDay,
+}: DeciDevelopmentPanelProps): JSX.Element {
+	return (
+		<>
+			<crumbs-p slot="title-5">Développement</crumbs-p>
+			<crumbs-panel slot="content-5" panel-title="Développement" style={{ maxHeight: "calc(100vh - 14.5rem)"}}>
+				<crumbs-p>
+					Date actuelle : {currentDay}
+				</crumbs-p>
+				<br />
+				<div style={{ display: "flex", gap: "1rem" }}>
+					<crumbs-button
+						title="Jour précédent"
+						onClick={onDecrementDay}
+						role="button"
+					>
+						← Jour précédent
+					</crumbs-button>
+					<crumbs-button
+						title="Jour suivant"
+						onClick={onIncrementDay}
+						role="button"
+					>
+						Jour suivant →
+					</crumbs-button>
+				</div>
+			</crumbs-panel>
+		</>
+	);
+}
+
--- front/src/components/DevelopmentPanelComponent.tsx
+++ front/src/components/DevelopmentPanelComponent.tsx
@@ -1,3 +1,4 @@
+import { DeciDevelopmentPanel } from "@deciphraze/ds";
 import { useDay } from "../hooks/useDay";
 import { useGameContext } from "../hooks/useGameContext";
 import { isDev } from "../utils/isDev";
@@ -21,31 +22,11 @@ export function DevelopmentPanelComponent(): JSX.Element | null {
 	}
 
 	return (
-		<>
-			<crumbs-p slot="title-5">Développement</crumbs-p>
-			<crumbs-panel slot="content-5" panel-title="Développement" style={{ maxHeight: "calc(100vh - 14.5rem)"}}>
-				<crumbs-p>
-					Date actuelle : {currentDay}
-				</crumbs-p>
-				<br />
-				<div style={{ display: "flex", gap: "1rem" }}>
-					<crumbs-button
-						title="Jour précédent"
-						onClick={handleDecrementDay}
-						role="button"
-					>
-						← Jour précédent
-					</crumbs-button>
-					<crumbs-button
-						title="Jour suivant"
-						onClick={handleIncrementDay}
-						role="button"
-					>
-						Jour suivant →
-					</crumbs-button>
-				</div>
-			</crumbs-panel>
-		</>
+		<DeciDevelopmentPanel
+			currentDay={currentDay}
+			onIncrementDay={handleIncrementDay}
+			onDecrementDay={handleDecrementDay}
+		/>
 	);
 };
 
