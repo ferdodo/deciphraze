cd41a062444e60460273898119947cfad1cace2f
Thomas Riffard <ferdodo@hotmail.fr>
2026-04-04 02:51:46 +0200

    move files and create initial immutable locks

--- persistance/src/repositories/AllAchievements/AchievementRepository.ts
+++
@@ -1,8 +0,0 @@
-import type { AllAchievementsV1 } from "./AllAchievementsV1";
-
-export interface AchievementRepository {
-	loadAchievements(): AllAchievementsV1;
-	saveAchievements(achievements: AllAchievementsV1): void;
-	subscribe(callback: (achievements: AllAchievementsV1) => void): () => void;
-	clear(): void;
-}
