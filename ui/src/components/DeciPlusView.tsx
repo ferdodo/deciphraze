38484da21beaf2023a93aad647e5a40d9333e6ed
Thomas Riffard <ferdodo@hotmail.fr>
2026-04-05 10:53:25 +0200

    use dvh

--- ui/src/components/DeciPlusView.tsx
+++ ui/src/components/DeciPlusView.tsx
@@ -10,7 +10,7 @@ interface DeciPlusViewProps {
 export function DeciPlusView({ title, content, onBack }: DeciPlusViewProps): React.JSX.Element {
 	return (
         <div style={{ display: "grid", placeContent: "center" }}>
-		<crumbs-panel panel-title={title} style={{ maxHeight: "calc(100svh - 8.5rem)" }}>
+		<crumbs-panel panel-title={title} style={{ maxHeight: "calc(100dvh - 8.5rem)" }}>
 			{content}
 			<div slot="footer">
 				<crumbs-button
