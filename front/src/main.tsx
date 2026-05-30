15ef43ead2039b96c91e72b5e1130fe60b9ebc0a
Thomas Riffard <ferdodo@hotmail.fr>
2026-05-30 16:25:06 +0200

    add css loader

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -11,3 +11,12 @@ const container: HTMLElement = document.createElement("div");
 document.body.appendChild(container);
 const root: Root = createRoot(container);
 root.render(<App />);
+const loaderContainer: HTMLElement | null = document.getElementById("html-loader");
+
+if (loaderContainer) {
+    loaderContainer.style.opacity = "0";
+
+    setTimeout(() => {
+        loaderContainer.remove();
+    }, 500);;
+}
